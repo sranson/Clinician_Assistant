6bf038eaa7c5ec2571a2e6c762356b7555b88507
Salome Ranson <salomeranson@gmail.com>
2021-10-04 14:30:43 -0400

    updated connection for remote database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/soap-note",
+    process.env.MONGODB_URI || "mongodb://localhost/soapnote",
     {
     useNewUrlParser: true,
     useUnifiedTopology: true,

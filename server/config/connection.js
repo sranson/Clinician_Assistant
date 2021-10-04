5cc1745863ff50fbde000630b1d0c3d334d1128e
Salome Ranson <salomeranson@gmail.com>
2021-10-04 14:00:37 -0400

    added server directory, server.js files, and connection.js file

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false, 
+    }
+);
+
+module.exports = mongoose.connection;
